bbe974dd43f9a424cef08619cf6066070e3e1333
郑健 <570470242@qq.com>
2020-09-21 00:14:55 +0800

    代码优化-1

--- src/main/java/com/zhengj/ZTtranswarpApplication.java
+++ src/main/java/com/zhengj/ZTtranswarpApplication.java
@@ -4,22 +4,13 @@ import com.zhengj.web.support.MvcConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
 @Import({ MvcConfiguration.class })
-public class ZTtranswarpApplication extends WebMvcConfigurationSupport {
+public class ZTtranswarpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZTtranswarpApplication.class, args);
 	}
-
-	@Override
-	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-		super.addResourceHandlers(registry);
-	}
-
 
 }
